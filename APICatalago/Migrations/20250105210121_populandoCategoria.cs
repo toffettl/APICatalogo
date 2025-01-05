9c594d829bcfe54cfaf9dcb1801e3c09c353dc0e
felipe <felip@DESKTOP-K14QK8H>
2025-01-05 18:05:12 -0300

    populando categoria

---
+++ APICatalago/Migrations/20250105210121_populandoCategoria.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalago.Migrations
+{
+    /// <inheritdoc />
+    public partial class populandoCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categoria(Nome, ImagemUrl) Values('Bebidas','Bebidas.jpg')");
+            migrationBuilder.Sql("Insert into Categoria(Nome, ImagemUrl) Values('Lanches','Lanches.jpg')");
+            migrationBuilder.Sql("Insert into Categoria(Nome, ImagemUrl) Values('Sobremesas','Sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+        }
+    }
+}
